85066a23a5a9ae62c43da1e6048878c429387acf
aleemsajjad1 <aleemsajjad1@gmail.com>
2023-06-25 14:06:14 +0500

    updates

--- src/components/layouts/registerBusiness-layout.tsx
+++ src/components/layouts/registerBusiness-layout.tsx
@@ -2,7 +2,6 @@ import { useRouter } from 'next/router';
 import Logo from '@/components/ui/logo';
 import React from 'react';
 import Image from 'next/image';
-import logo from '../../../public/image/leSouq.png'
 
 export default function RegisterBusPageLayout({
   children,
@@ -11,16 +10,6 @@ export default function RegisterBusPageLayout({
   // const dir = locale === 'ar' || locale === 'he' ? 'rtl' : 'ltr';
 
   return (
-    <div
-      className="flex h-screen items-center justify-center bg-light sm:bg-gray-100"
-    >
-      <div className="m-auto w-full max-w-[820px]  rounded bg-light p-5 sm:p-8 sm:shadow">
-        <div className="mb-2 flex justify-center">
-          {/* <Logo /> */}
-          <Image width={250} height={100} src={logo} />
-        </div>
-        {children}
-      </div>
-    </div>
+    <div className="flex h-screen items-center justify-center bg-light sm:bg-gray-100"></div>
   );
 }
